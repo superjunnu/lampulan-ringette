d6e3d95e370ebd11999efc48b22111b3479cdf71
Hannu Kulmala <hannu.kulmala@edu.sasky.fi>
2022-06-14 16:24:24 +0300

    Kalenteriin otsikko ja viikonpäivien nimet

--- calendar.js
+++ calendar.js
@@ -9,8 +9,17 @@ function display() {
    console.log(`days in current month = ${days_in_current_month}`);
    let day = 1; 
    let empty_days_at_start = new Date(current_year, current_month, day).getDay() - 1;
-   let header = today.toLocaleString('default', { month: 'long' });
-   calendarHTML += `<div class="header"><h2>${header}</h2></div>`;
+   let header = today.toLocaleString('fi-FI', { month: 'long' });
+   calendarHTML += `<div class="month-header"><h2>${header}</h2></div>`;
+   calendarHTML += `<div class="weekday-header-container">`;
+   calendarHTML += `<div class="weekday-header"><h3>Maanantai</h3></div>`;
+   calendarHTML += `<div class="weekday-header"><h3>Tiistai</h3></div>`;
+   calendarHTML += `<div class="weekday-header"><h3>Keskiviikko</h3></div>`;
+   calendarHTML += `<div class="weekday-header"><h3>Torstai</h3></div>`;
+   calendarHTML += `<div class="weekday-header"><h3>Perjantai</h3></div>`;
+   calendarHTML += `<div class="weekday-header"><h3>Lauantai</h3></div>`;
+   calendarHTML += `<div class="weekday-header"><h3>Sunnuntai</h3></div>`;
+   calendarHTML += `</div>`;
    calendarHTML += `<div class="month"><div class="week">`;
    for (let j = 0; j < empty_days_at_start; j++) {
       calendarHTML += `<div class="day empty"></div>`;
